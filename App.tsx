10377ea5b0b3e59a678c655afa9159a25a146b4a
Vladimir <vladistar200302@gmail.com>
2022-01-28 09:18:10 -0500

    feat: firts commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Text>VOJES APP</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
